45e8cad71fd2b55368cc5c2ea23c01a4758f15f8
Faroukkooli <Mohamedfarouk.kooli@medtech.tn>
2022-05-31 12:28:20 +0100

    talaali f note

---
+++ filter.js
@@ -0,0 +1,6 @@
+import { TextInput } from 'react-admin';
+
+const postFilters = [
+    <TextInput label="Search" source="q" alwaysOn />,
+    <TextInput label="Title" source="title" defaultValue="Name of the job you want" />,
+];
---
+++ removeuser.js
@@ -0,0 +1,6 @@
+const isAdmin = props.user.authority === "ADMIN";
+/*const [isAuthorized,setIsAuthorized] = useState(['ADMIN'].includes(props.user.authority));
+useEffect(()=>{
+    setIsAuthorized(['ADMIN'].includes(props.user.authority))
+   },[props.user.authority]) */
+
